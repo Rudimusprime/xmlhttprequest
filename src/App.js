8e2a54134aaac381c0fbcdf7a56d9538f86cd2f2
DanielB <d.banasiewicz@pdo.com.pl>
2021-11-03 20:17:54 +0100

    restAPI fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
 class App extends Component {
+
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     users: []
+  //   }
+  //   this.requestData()
+  // }
+
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    // this.requestData()
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+
+    // xhr.onload = () => {
+    //   console.log(xhr.status);
+    //   if (xhr.status === 200) {
+    //     const users = JSON.parse(xhr.response)
+    //     console.log(users);
+    //     this.setState({ users })
+    //   }
+    //   // console.log(typeof xhr.response);
+    // }
+
+    xhr.addEventListener('load', () => {
+      console.log(xhr.status);
+      if (xhr.status === 200) {
+        const users = JSON.parse(xhr.response)
+        console.log(users);
+        this.setState({users})
+      }
+
+    })
+
+    xhr.send(null)
+  }
+
+
+  // requestData() {
+  //   const xhr = new XMLHttpRequest();
+  //   xhr.open('GET', 'https://jsonplaceholder.typicode.com/users', true);
+
+  //   xhr.onload = () => {
+  //     console.log(xhr.status);
+  //     if (xhr.status === 200) {
+  //       const users = JSON.parse(xhr.response)
+  //       console.log(users);
+  //       this.setState({ users })
+  //     }
+  //     // console.log(typeof xhr.response);
+  //   }
+  //   xhr.send(null)
+  // }
+
   render() {
+    const users = this.state.users.map(user => (
+        <div key={user.id}>
+          <h4>{user.name}</h4>
+          <p>{user.address.city}</p>
+        </div>
+    ))
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+          {users}
+        </div>
     );
   }
 }
